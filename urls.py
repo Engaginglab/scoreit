90a0414eff2f53ba5dfd70e72c2237ac6ffc86be
Martin Kleinschrodt <martin@maklesoft.com>
2012-07-11 17:42:58 +0200

    Move api url definition to handball app

--- urls.py
+++ urls.py
@@ -1,22 +1,10 @@
 from django.contrib import admin
 from django.conf.urls.defaults import *
-from tastypie.api import Api
-from handball.api import *
 
 
 admin.autodiscover()
 
-v1_api = Api(api_name='v1')
-v1_api.register(UnionResource())
-v1_api.register(ClubResource())
-v1_api.register(TeamResource())
-v1_api.register(UserResource())
-v1_api.register(PersonResource())
-v1_api.register(GameResource())
-v1_api.register(LeagueResource())
-
 urlpatterns = patterns('',
     (r'^', include('handball.urls')),
-    (r'^api/', include(v1_api.urls)),
     (r'^admin/', include(admin.site.urls))
 )

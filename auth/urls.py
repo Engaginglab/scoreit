8f22efd82ac07124353359960bf41f5b6430ee5c
Martin Kleinschrodt <martin@maklesoft.com>
2012-08-04 19:16:11 +0200

    leave away api/ prefix in urls (since everything belongs to the api)

--- auth/urls.py
+++ auth/urls.py
@@ -8,14 +8,14 @@ v1_api.register(UserResource())
 v1_api.register(ProfileResource())
 
 urlpatterns = patterns('auth.views',
-    (r'^activate/([abcdef0123456789]+)$', 'activate')
+    (r'^v1/activate/([abcdef0123456789]+)$', 'activate')
 )
 
-urlpatterns += patterns('', (r'^api/', include(v1_api.urls)))
+urlpatterns += patterns('', (r'^', include(v1_api.urls)))
 
 # Non-resource api endpoints
 urlpatterns += patterns('auth.api',
-    (r'^api/v1/validate/$', 'validate_user'),
-    (r'^api/v1/unique/$', 'is_unique'),
-    (r'^api/v1/signup/$', 'sign_up')
+    (r'^v1/validate/$', 'validate_user'),
+    (r'^v1/unique/$', 'is_unique'),
+    (r'^v1/signup/$', 'sign_up')
 )
